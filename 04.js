e06c92fedc205a65e5e24917c205de14c51f77e5
itheima <1269827945@qq.com>
2024-04-29 19:32:08 +0800

    nodeday1

--- 04.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const tail =path.basename('/a/b/c/index.html')
-console.log(tail);
-const name = path.basename(tail,'.html')
-console.log(name);
-const name2 = path.extname(tail)
-console.log(name2);
